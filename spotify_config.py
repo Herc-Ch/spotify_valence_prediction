34cd19229e9be938ba7c9936a9b264677ac101e3
Herc-Ch <134635471+Herc-Ch@users.noreply.github.com>
2023-07-13 20:31:03 +0300

    Add files via upload

---
+++ spotify_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+config = {
+    'client_id' : '9762084f49744a17b6bf2faf8b8329a6',
+    'client_secret' : 'b61ae48a2f5e4678b94d10cf9947db23'
+}
+
